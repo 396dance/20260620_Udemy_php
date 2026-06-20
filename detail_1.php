07ea010596fd600a21e4a03a4830b6b793c8f72f
mikuno yasuhiro <396dance@gmail.com>
2026-06-20 11:10:36 +0900

    各ページ作成

---
+++ detail_1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>問題1</h1>
+  <h2>問題文：あああああああああああああ</h2>
+  <li>
+    <ol>選択肢</ol>
+    <ol>選択肢</ol>
+    <ol>選択肢</ol>
+    <ol>選択肢</ol>
+  </li>
+  <p>答え：</p>
+  <div>解説あああああああああああああああああああああ</div>
+  <div><a href="./index.php">一覧へ戻る</a></div>
+</body>
+</html>
---
+++ detail_2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  詳細ページ2
+  <div><a href="./index.php">戻る</a></div>
+</body>
+</html>
