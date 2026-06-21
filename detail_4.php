709c04619420ab409285bae7c57b28b4539e5033
mikuno yasuhiro <396dance@gmail.com>
2026-06-21 12:00:06 +0900

    問題文の表示

--- detail_4.php
+++ detail_4.php
@@ -1,12 +1,33 @@
+<?php
+// 1. まずは自分でCSVを読み込んでデータを準備する
+$fp = fopen(__DIR__ . "/data.csv", "r");
+$questions = [];
+while ($data = fgetcsv($fp, escape: "\\")) {
+  $questions[] = $data;
+}
+fclose($fp);
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
-  詳細ページ4
-  <div><a href="./index.php">戻る</a></div>
+  <h1>問題<?php echo $questions[4][0] ?></h1>
+  <h2><?php echo $questions[4][1] ?></h2>
+  <li>
+    <ol>a：<?php echo $questions[4][2] ?></ol>
+    <ol>b：<?php echo $questions[4][3] ?></ol>
+    <ol>c：<?php echo $questions[4][4] ?></ol>
+    <ol>d：<?php echo $questions[4][5] ?></ol>
+  </li>
+  <p>答え：<?php echo $questions[4][6] ?></p>
+  <div>解説：<?php echo $questions[4][7] ?></div>
+  <div><a href="./template/index.tmp.php">一覧へ戻る</a></div>
 </body>
+
 </html>
